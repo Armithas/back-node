3c5bdfe50f65181fbddc73457d446fd637665012
Jorge Armas <jorge_armasmartinez@jafra.com.mx>
2023-02-10 23:50:34 -0600

    comando start añadido

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
 
 
-const sequelize = new Sequelize('reto', 'root', 'adminTareas08,,', {
-    host: 'localhost',
-    port: 3308,
+const sequelize = new Sequelize('railway', 'root', 'RacVeCwPWfNb81g9e4E9', {
+    host: 'containers-us-west-179.railway.app',
+    port: 7527,
     dialect: 'mysql',   
 });
+
+// const sequelize = new Sequelize('reto', 'root', 'adminTareas08,,', {
+//     host: 'localhost',
+//     port: 3308,
+//     dialect: 'mysql',   
+// });
 
 export default sequelize;
